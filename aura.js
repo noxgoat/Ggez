76e660092cd363e384e871991b71598260307fe2
noxgoat <capetinhadomine@gmail.com>
2026-03-18 13:38:13 -0300

    Update aura.js

--- aura.js
+++ aura.js
@@ -1,19 +1,5 @@
 module.exports = {
-    onLoad: () => {
-        console.log("Plugin carregado");
-    },
-
-    patches: [
-        {
-            find: "getCurrentUser",
-            replacement: {
-                match: /return (\w)/,
-                replace: (_, orig) => `return Object.assign({}, ${orig}, {
-                    username: "e",
-                    globalName: "e",
-                    id: "246292202186211328"
-                })`
-            }
-        }
-    ]
+    onLoad() {
+        console.log("funcionou");
+    }
 };
